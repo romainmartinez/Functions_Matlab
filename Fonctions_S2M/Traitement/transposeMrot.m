ccccb83229d7c8200fb71e054f0ef5acbb8ce3f8
Romain Martinez <martinez.staps@gmail.com>
2016-10-27 14:51:31 -0400

    delete useless functions

--- Fonctions_S2M/Traitement/transposeMrot.m
+++
@@ -1,5 +0,0 @@
-function out = transposeMrot(M)
-    R = M(1:3,1:3);
-    T = M(1:3,4);
-    out = [R', -R'*T; 0 0 0 1]; % 0 vers la boite   
-end
--- Fonctions_S2M/save_load/loadMatMVCMat.m
+++
@@ -1,4 +0,0 @@
-function [MatMVCMat]= loadMatMVCMat(Sujet) %#ok<STOUT>
-    %load
-    load(['./Resultats/' Sujet '/MaxMVC' Sujet '.mat']);
-end
