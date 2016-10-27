3812af060fa63d78854b0077116626f680bb1f4b
Romain Martinez <martinez.staps@gmail.com>
2016-10-27 12:31:58 -0400

    Try to get Fnorm

--- getforcedata.m
+++ getforcedata.m
@@ -1,18 +1,27 @@
-function [Data] = getforcedata(Alias)
-    % Dossier des essais
+function [Force] = getforcedata(Alias)
+%% Dossier des essais
 folderPath = ['\\10.89.24.15\f\Data\Shoulder\RAW\IRSST_' Alias.sujet 'd\trials\'];
-    % noms des fichiers c3d
+%% noms des fichiers c3d
 C3dfiles = dir([folderPath '*.c3d']);
-
-    % Ouvertures des c3d analogiques (EMG & Force)
+%% Matrice d'étalonnage
+EtalonnageForce=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
+                 208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
+                 227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
+                 5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
+                 5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
+                 -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
+%% Ouvertures des c3d analogiques (EMG & Force)
     for i = 1 : length(C3dfiles)
         FileName = [folderPath C3dfiles(i).name]; 
         btkc3d = btkReadAcquisition(FileName);
         btkanalog = btkGetAnalogs(btkc3d);
-        
+%% Obtenir la force brute   
         for f = 1 : 6
-            Data(i).force(:,f) = getfield(btkanalog, ['Voltage_' num2str(f)] );
+            Force(i).Raw(:,f) = getfield(btkanalog, ['Voltage_' num2str(f)] );
         end
+%% Étalonnage du capteur
+% Feta= Force(i).Raw*EtalonnageForce';
     end
+
     
 end
