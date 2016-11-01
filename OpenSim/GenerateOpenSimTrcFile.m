1b2ece66a6361f849889ed2e2c7cc52e62822d64
Romain Martinez <martinez.staps@gmail.com>
2016-11-01 11:03:35 -0400

    Improvement : speed & simplicity

--- OpenSim/GenerateOpenSimTrcFile.m
+++ OpenSim/GenerateOpenSimTrcFile.m
@@ -1,46 +1,19 @@
-function GenerateOpenSimTrcFile(FileMot,confTrc,markers)
-
-    fid   = fopen([FileMot '.trc'],'w+');
-
-
-for i = 1:length(confTrc.header1)
-    fprintf(fid,'%s\t',confTrc.header1{i});
-end
-
-fprintf(fid,'\n');
-
-for i = 1:length(confTrc.header2)
-    fprintf(fid,'%s\t',confTrc.header2{i});
-end
-
-fprintf(fid,'\n');
-
-for i = 1:length(confTrc.header3)
-    fprintf(fid,'%s\t',confTrc.header3{i});
-end
-
-fprintf(fid,'\n');
-
-for i = 1:length(confTrc.header4)
-    fprintf(fid,'%s\t',confTrc.header4{i});
-end
-
-fprintf(fid,'\n');
-
-for i = 1:length(confTrc.header5)
-    fprintf(fid,'%s\t',confTrc.header5{i});
-end
-
-fprintf(fid,'\n');
-
-
-    
-%     for i =1:size(markers,2) %nb instants
-%         for j = 1:size(markers,1) %nb markers
-%             fprintf(fid,'%1.10f',markers(j,i));
-%             fprintf(fid,'\t','');
-%         end
-%         fprintf(fid,'\n');
-%     end
-    
+function GenerateOpenSimTrcFile(exportPath,trialname,confTrc,trcmat)
+%% Ouverture
+    fid   = fopen([exportPath trialname '.trc'],'w+');
+%% Écriture des headers
+    fprintf(fid,'%s\t',confTrc.header1{:});
+    fprintf(fid,'\n');
+    fprintf(fid,'%s\t',confTrc.header2{:});
+    fprintf(fid,'\n');
+    fprintf(fid,'%s\t',confTrc.header3{:});
+    fprintf(fid,'\n');
+    fprintf(fid,'%s\t',confTrc.header4{:});
+    fprintf(fid,'\n');
+    fprintf(fid,'%s\t',confTrc.header5{:});
+    fprintf(fid,'\n');
+%% Écriture des datas
+    fprintf(fid, [repmat('%f\t', 1, size(trcmat, 2)) '\n'], trcmat');
+%% Fermeture   
+    fclose(fid);
 end
