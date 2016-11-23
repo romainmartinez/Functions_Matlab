f22966507aef122a8224b2c7fd38e2f9152f70db
Romain Martinez <martinez.staps@gmail.com>
2016-11-23 16:31:16 -0500

    Minor changes

--- forceinglobal.m
+++ forceinglobal.m
@@ -48,25 +48,25 @@ matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
                  5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
                  5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
                  -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
-forcemoment    = matrixetal * voltage;
+forcemoment = matrixetal * voltage;
 
     % Reshape de la matrice de force
 forcemoment = reshape(forcemoment, [6, 1, size(forcemoment,2)]);
-ratioFreq = size(forcemoment, 3) / size(RT,3);
+ratioFreq   = size(forcemoment, 3) / size(RT,3);
 
     % Forces dans le global
-forcein0       = multiprod(RT_Trans(1:3,1:3,:), forcemoment(1:3,:,1:ratioFreq:end));
+forcein0    = multiprod(RT_Trans(1:3,1:3,:), forcemoment(1:3,:,1:ratioFreq:end));
 
     % Moments dans le global (/!\ moment exprimés au centre du capteur /!\)
-momentin0      = multiprod(RT_Trans(1:3,1:3,:), forcemoment(4:6,:,1:ratioFreq:end));
+momentin0   = multiprod(RT_Trans(1:3,1:3,:), forcemoment(4:6,:,1:ratioFreq:end));
 
     % Moments exprimés dans le global     
-momentin0 = momentin0 + cross(RT(1:3,4,:), forcein0);
+momentin0   = momentin0 + cross(RT(1:3,4,:), forcein0);
 
     % Matrice 3D vers 2D
-forcein0  = transpose(squeeze(forcein0));
-momentin0 = transpose(squeeze(momentin0));
-
+forcein0    = transpose(squeeze(forcein0));
+momentin0   = transpose(squeeze(momentin0));
+ 
     % Rebase force et moment
     for j =1:3
         forcein0(:,j)  = forcein0(:,j)  - mean(forcein0(1:100,j));
