48264a6f009ee33dc82523b589a9d586322b6c60
Romain Martinez <martinez.staps@gmail.com>
2016-11-25 11:48:20 -0500

    Add condition number

--- getcondition.m
+++ getcondition.m
@@ -9,23 +9,64 @@ for essai = 1 : length(Data)
     elseif strcmp(Data(essai).trialname(2:4),'12H') == 1
         Data(essai).poids = 12;                             % 12kg
     elseif strcmp(Data(essai).trialname(2:4),'18H') == 1
-        Data(essai).poids = 18;                             % 18kg  
+        Data(essai).poids = 18;                             % 18kg
     end
     
     %% Boucle pour la hauteur
     for hauteur = 1 : 6
         if strcmp(Data(essai).trialname(end-2),num2str(hauteur)) == 1
-            Data(essai).hauteur = hauteur;  
+            Data(essai).hauteur = hauteur;
         end
     end
     
+    %% Boucle pour déterminer la condition
+    if      Data(essai).poids == 6
+        
+        if     Data(essai).hauteur == 1
+            Data(essai).condition = 1;
+        elseif Data(essai).hauteur == 2
+            Data(essai).condition = 2;
+        elseif Data(essai).hauteur == 3
+            Data(essai).condition = 3;
+        elseif Data(essai).hauteur == 4
+            Data(essai).condition = 4;
+        elseif Data(essai).hauteur == 5
+            Data(essai).condition = 5;
+        elseif Data(essai).hauteur == 6
+            Data(essai).condition = 6;
+        end
+        
+    elseif  Data(essai).poids == 12
+        
+        if     Data(essai).hauteur == 1
+            Data(essai).condition = 7;
+        elseif Data(essai).hauteur == 2
+            Data(essai).condition = 8;
+        elseif Data(essai).hauteur == 3
+            Data(essai).condition = 9;
+        elseif Data(essai).hauteur == 4
+            Data(essai).condition = 10;
+        elseif Data(essai).hauteur == 5
+            Data(essai).condition = 11;
+        elseif Data(essai).hauteur == 6
+            Data(essai).condition = 12;
+        end
+        
+    elseif  Data(essai).poids == 18
+        
+        if     Data(essai).hauteur == 1
+            Data(essai).condition = 13;
+        elseif Data(essai).hauteur == 2
+            Data(essai).condition = 14;
+        elseif Data(essai).hauteur == 3
+            Data(essai).condition = 15;
+        elseif Data(essai).hauteur == 4
+            Data(essai).condition = 16;
+        elseif Data(essai).hauteur == 5
+            Data(essai).condition = 17;
+        elseif Data(essai).hauteur == 6
+            Data(essai).condition = 18;
+        end
+        
+    end   
 end
-
-
-    
-    
-    
-    
-    
-end
-
