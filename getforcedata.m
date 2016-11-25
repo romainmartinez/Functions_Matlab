9e03dadf6baa8a04c5fa3544cdd1fa2f8e30113a
Romain Martinez <martinez.staps@gmail.com>
2016-11-25 11:24:59 -0500

    export one cell variable instead of multiple variable

--- getforcedata.m
+++ getforcedata.m
@@ -1,4 +1,4 @@
-function [Force] = getforcedata(subject)
+function [forceindex] = getforcedata(subject)
 %% Dossier des essais
 folderPath = ['\\10.89.24.15\f\Data\Shoulder\RAW\IRSST_' subject 'd\trials\'];
 
@@ -17,16 +17,15 @@ for     i  = 1 : length(C3dfiles)
     end
     %% Étalonnage
     matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
-        208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
-        227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
-        5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
-        5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
-        -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
+                208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
+                227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
+                5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
+                5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
+                -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
     
     Force_eta= Force_Raw * matrixetal';
     
     %% Rebase
-    Forcerebase = [];
     for j =1:6
         Force_rebase(:,j) = Force_eta(:,j)-mean(Force_eta(1:100,j));
     end
@@ -40,9 +39,11 @@ for     i  = 1 : length(C3dfiles)
     % Détection de la prise (>5 N)
     threshold  = 5;
     index      = find(Force_norm > threshold);
-    premier    = index(1);
-    dernier    = index(end);
-    
+    forceindex{i,1}    = index(1);
+    forceindex{i,2}    = index(end);
+    forceindex{i,3}    = FileName(54:end-4);
+
+clearvars FileName btkc3d btkanalog Force_Raw Force_eta Force_rebase Force_filt Force_norm index  
 end
 
 end
