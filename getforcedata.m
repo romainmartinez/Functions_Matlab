4b2d4535c5532ff9b111b0cef4f0bd5842334a26
Romain Martinez <martinez.staps@gmail.com>
2016-11-29 14:44:55 -0500

    re-show GUI if oldlabel ~= channels

--- getforcedata.m
+++ getforcedata.m
@@ -5,31 +5,45 @@ folderPath = ['\\10.89.24.15\f\Data\Shoulder\RAW\IRSST_' subject 'd\trials\'];
 %% noms des fichiers c3d
 C3dfiles   = dir([folderPath '*.c3d']);
 
+%% premier itération
+iter = 1;
+
 %% Ouvertures des c3d analogiques (EMG & Force)
 for     i  = 1 : length(C3dfiles)
     FileName   = [folderPath C3dfiles(i).name];
     btkc3d     = btkReadAcquisition(FileName);
     btkanalog  = btkGetAnalogs(btkc3d);
     %% Interface pour sélectionner le nom des channels de force
-    if i == 1
-        
-        matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
-                208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
-                227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
-                5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
-                5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
-                -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
-            
-       fields   = fieldnames(btkanalog);
-       channels = {'Voltage_1','Voltage_2','Voltage_3','Voltage_4','Voltage_5','Voltage_6'};
-       [oldlabel] = GUI_renameforce(fields, channels)
-       pause
+    while(true)
+        %% Boucle pour le premier essai
+        switch iter
+            case 1
+            matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
+                    208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
+                    227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
+                    5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
+                    5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
+                    -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
+
+           fields   = fieldnames(btkanalog);
+           channels = {'Voltage_1','Voltage_2','Voltage_3','Voltage_4','Voltage_5','Voltage_6'};
+           [oldlabel] = GUI_renameforce(fields, channels)
+           pause
+        end
+
+        %% Si il n'y a pas de channel correspondant, relance le GUI
+        matches = strfind(fieldnames(btkanalog),oldlabel{1,1});
+        if any(vertcat(matches{:}))
+            iter = 2;
+            break
+        else
+            iter = 1;
+        end
     end
-    %% Obtenir la force brute
-    Force_Raw = [];
-    for f = 1 : length(oldlabel)
-        Force_Raw(:,f) = getfield(btkanalog,char(oldlabel{1,f}));
-    end
+%% Obtenir la force brute
+for f = 1 : length(oldlabel)
+    Force_Raw(:,f) = getfield(btkanalog,char(oldlabel{1,f}));
+end
     %% Étalonnage
     Force_eta= Force_Raw * matrixetal';
     
