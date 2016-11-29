dcfa6c13e6fbcec3130273f10c7c6f7e290eec86
Romain Martinez <martinez.staps@gmail.com>
2016-11-29 10:43:26 -0500

    Fix issue when retrieve the oldlabel variable from GUI workspace

--- getforcedata.m
+++ getforcedata.m
@@ -12,26 +12,29 @@ for     i  = 1 : length(C3dfiles)
     btkanalog  = btkGetAnalogs(btkc3d);
     %% Interface pour sélectionner le nom des channels de force
     if i == 1
-       fields   = fieldnames(btkanalog);
-       channels = {'Voltage_1','Voltage_2','Voltage_3','Voltage_4','Voltage_5','Voltage_6'};
-       [oldlabel] = GUI_renameforce(fields, channels)
-       
-    end
-    %% Obtenir la force brute
-    for f = 1 : 6
-        Force_Raw(:,f) = getfield(btkanalog, ['Voltage_' num2str(f)] );
-    end
-    %% Étalonnage
-    matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
+        
+        matrixetal=[15.7377 -178.4176 172.9822 7.6998 -192.7411 174.1840;
                 208.3629 -109.1685 -110.3583  209.3269 -104.9032 -103.5278;
                 227.6774 222.8613 219.1087 234.3732 217.1453 221.2831;
                 5.6472 -0.7266 -0.3242 5.4650 -8.9705 -8.4179;
                 5.7700 6.7466 -6.9682 -4.1899 1.5741 -2.4571;
                 -1.2722 1.6912 -3.0543 5.1092 -5.6222 3.3049];
-    
+            
+       fields   = fieldnames(btkanalog);
+       channels = {'Voltage_1','Voltage_2','Voltage_3','Voltage_4','Voltage_5','Voltage_6'};
+       [oldlabel] = GUI_renameforce(fields, channels)
+       pause
+    end
+    %% Obtenir la force brute
+    Force_Raw = [];
+    for f = 1 : length(oldlabel)
+        Force_Raw(:,f) = getfield(btkanalog,char(oldlabel{1,f}));
+    end
+    %% Étalonnage
     Force_eta= Force_Raw * matrixetal';
     
     %% Rebase
+    Force_rebase = [];
     for j =1:6
         Force_rebase(:,j) = Force_eta(:,j)-mean(Force_eta(1:100,j));
     end
@@ -42,8 +45,9 @@ for     i  = 1 : length(C3dfiles)
     % Norme de la force
     Force_norm = sqrt(sum(Force_filt.^2,2));
     Force_norm = Force_norm(1:end-50);
+    
     % Détection de la prise (>5 N)
-    threshold = 5;
+    threshold =  5;
     index     = find(Force_norm > threshold);
     forceindex{i,1} = index(1);
     forceindex{i,2} = index(end);
