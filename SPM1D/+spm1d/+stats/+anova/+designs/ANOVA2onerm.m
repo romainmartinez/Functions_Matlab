9051160ce2917e839398faf24dfdd74a3e031b2a
Romain Martinez <martinez.staps@gmail.com>
2016-11-29 18:08:28 -0500

    add spm1d package

---
+++ SPM1D/+spm1d/+stats/+anova/+designs/ANOVA2onerm.m
@@ -0,0 +1,55 @@
+%__________________________________________________________________________
+% Copyright (C) 2016 Todd Pataky
+
+
+
+
+classdef ANOVA2onerm < spm1d.stats.anova.designs.ANOVA2rm
+    methods
+        function self = ANOVA2onerm(A, B, SUBJ)
+            self@spm1d.stats.anova.designs.ANOVA2rm(A, B, SUBJ)
+            self.S = spm1d.stats.anova.factors.FactorNested(SUBJ, self.A);
+            self.effect_labels = {'Main A', 'Main B', 'Interaction AB'};
+            self.term_labels = {'Intercept', 'A', 'B', 'S', 'AB', 'SB'};
+            self.f_terms = {{'A','S'}, {'B','SB'}, {'AB','SB'}};
+            self   = assemble(self);
+            self.check_balanced()
+        end
+    end
+    
+    methods (Access = private)
+        function self = assemble(self)
+            %assemble design matrix columns:
+            XCONST  = self.get_column_const();
+            XA      = self.A.get_design_main();
+            XB      = self.B.get_design_main();
+            XS      = self.S.get_design_main();
+            XAB     = self.A.get_design_interaction(self.B);
+            XSB     = self.S.get_design_interaction(self.B);
+            %build model:
+            model   = spm1d.stats.anova.ModelBuilder(self.term_labels);
+            model   = model.add_main_columns('Intercept', XCONST);
+            model   = model.add_main_columns('A', XA);
+            model   = model.add_main_columns('B', XB);
+            model   = model.add_main_columns('S', XS);
+            model   = model.add_main_columns('AB', XAB);
+            model   = model.add_main_columns('SB', XSB);
+            self.X  = model.get_design_matrix();
+            self.contrasts = model.get_contrasts;
+        end
+        
+        function check_balanced(self)
+            if ~(self.B.balanced && self.S.balanced)
+                error('Design must be balanced.')
+            elseif ~self.S.check_balanced_rm(self.A)
+                error('Design must be balanced.')
+            elseif ~self.S.check_balanced_rm(self.B)
+                error('Design must be balanced.')
+            end
+        end
+
+        
+    end
+
+    
+end
