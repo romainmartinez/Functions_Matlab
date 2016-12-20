e8ab7abb821c8ba1ff252fd12c2a39a248344ea8
Romain Martinez <martinez.staps@gmail.com>
2016-12-20 11:00:57 -0800

    Update S2Mlibpick

--- loadS2MLib.m
+++ loadS2MLib.m
@@ -1,4 +1,4 @@
 function loadS2MLib()
-    
-    run('\\10.89.24.15\e\Librairies\S2M_Lib\S2MLibPicker.m')
+% This function aim to load the S2M lib
+run('\\10.89.24.15\e\Librairies\S2M_Lib\S2MLibPicker.m')
 end
