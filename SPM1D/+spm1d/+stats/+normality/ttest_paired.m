dafb8f4a3c5d1f0e6d3b473daf6d47b697490e67
Romain Martinez <martinez.staps@gmail.com>
2016-12-20 09:39:18 -0800

    clone spm1dmatlab from github

--- SPM1D/+spm1d/+stats/+normality/ttest_paired.m
+++
@@ -1,6 +0,0 @@
-function [SPM] = ttest_paired(YA, YB)
-%__________________________________________________________________________
-% Copyright (C) 2016 Todd Pataky
-
-
-SPM           = spm1d.stats.normality.ttest( YA - YB );
--- SPM1D/+spm1d/+stats/hotellings_paired.m
+++
@@ -1,9 +0,0 @@
-function [SPM] = hotellings_paired(YA, YB, varargin)
-%__________________________________________________________________________
-% Copyright (C) 2016 Todd Pataky
-
-
-
-SPM = spm1d.stats.hotellings(YB-YA, varargin{:});
-
-
